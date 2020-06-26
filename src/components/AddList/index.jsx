272d7cc5393546b916004a9e88b4729d22891725
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-06-26 22:36:44 +0300

    #3 Sidebar(AddList: value, color, RemoveList)

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -6,9 +6,31 @@ import closeImg from '../../assets/img/close.svg';
 
 import './AddList.scss';
 
-const AddList = ({ colors }) => {
-    const [visiblePopup, setVisiblePopup] = useState(true);
+const AddList = ({ colors, eAddList }) => {
+    const [visiblePopup, setVisiblePopup] = useState(false);
     const [selectedColor, selectColor] = useState(colors[0].id);
+    const [inputValue, setInputValue] = useState('');
+    
+    const onClose = () => {
+        setInputValue('');
+        selectColor(colors[0].id);
+        setVisiblePopup(false);
+    };
+
+    const addList = () => {
+        if (!inputValue) {
+            alert('Введите название списка');
+            return;
+        }
+
+        eAddList({ 
+            id: Math.random(), 
+            name: inputValue, 
+            color: colors.filter(c => c.id === selectedColor)[0].name 
+        });
+        onClose();
+    };
+
     return (
         <div className="add-list">
             <List 
@@ -25,18 +47,18 @@ const AddList = ({ colors }) => {
                                 xmlns="http://www.w3.org/2000/svg"
                             >
                                 <path 
-                                d="M8 1V15" 
-                                stroke="black" 
-                                strokeWidth="2" 
-                                strokeLinecap="round" 
-                                strokeLinejoin="round"
+                                    d="M8 1V15" 
+                                    stroke="black" 
+                                    strokeWidth="2" 
+                                    strokeLinecap="round" 
+                                    strokeLinejoin="round"
                                 />
                                 <path 
-                                d="M1 8H15" 
-                                stroke="black" 
-                                strokeWidth="2" 
-                                strokeLinecap="round" 
-                                strokeLinejoin="round"
+                                    d="M1 8H15" 
+                                    stroke="black" 
+                                    strokeWidth="2" 
+                                    strokeLinecap="round" 
+                                    strokeLinejoin="round"
                                 />
                             </svg>
                         ),
@@ -47,12 +69,18 @@ const AddList = ({ colors }) => {
             {visiblePopup && (
                 <div className="add-list__popup">
                     <img 
-                        onClick={() => setVisiblePopup(false)}
+                        onClick={onClose}
                         src={closeImg} 
                         alt="Закрыть" 
                         className="add-list__popup-closeBtn"
                     />
-                    <input className="field" type="text" placeholder="Название списка"/>
+                    <input 
+                        className="field" 
+                        value={inputValue} 
+                        onChange={e => setInputValue(e.target.value)}
+                        type="text" 
+                        placeholder="Название списка"
+                    />
                     <div className="add-list__popup-colors">
                        {
                            colors.map(color => (
@@ -65,7 +93,7 @@ const AddList = ({ colors }) => {
                            ))
                        }
                     </div>
-                    <button className="button">Добавить</button>
+                    <button onClick={addList} className="button">Добавить</button>
                 </div>
             )}
         </div>
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,9 +3,18 @@ import classNames from 'classnames';
 
 import Badge from '../Badge'
 
+import removeImg from '../../assets/img/remove.svg';
+
 import './List.scss';
 
-const List = ({ items, isRemovable, click }) => {
+const List = ({ items, isRemovable, click, onRemove }) => {
+
+    const removeList = (item) => {
+        if (window.confirm('Вы действительно хотите удалить список?')) {
+            onRemove(item);
+        }
+    }
+
     return (
     <ul onClick={click} className="list">
         {
@@ -13,6 +22,14 @@ const List = ({ items, isRemovable, click }) => {
             <li key={index} className={classNames(item.className, {'active': item.active})}>
                 <i>{ item.icon ? item.icon : <Badge color={item.color} /> }</i>
                 <span>{item.name}</span>
+                {isRemovable && (
+                    <img 
+                        className="list__remove-icon" 
+                        onClick={() => removeList(item)}
+                        src={removeImg} 
+                        alt="Закрыть" 
+                    />
+                )}
             </li>
             ))}
     </ul>
