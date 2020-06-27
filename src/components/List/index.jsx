ccf15ba6b2ed9ed84d00862b45765da2b5ff4f23
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-06-27 18:27:49 +0300

    #4 Tasks List (header, row task)

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -16,23 +16,24 @@ const List = ({ items, isRemovable, click, onRemove }) => {
     }
 
     return (
-    <ul onClick={click} className="list">
-        {
-            items.map((item, index) => (
-            <li key={index} className={classNames(item.className, {'active': item.active})}>
-                <i>{ item.icon ? item.icon : <Badge color={item.color} /> }</i>
-                <span>{item.name}</span>
-                {isRemovable && (
-                    <img 
-                        className="list__remove-icon" 
-                        onClick={() => removeList(item)}
-                        src={removeImg} 
-                        alt="Закрыть" 
-                    />
-                )}
-            </li>
-            ))}
-    </ul>
+        <ul onClick={click} className="list">
+            {
+                items.map((item, index) => (
+                    <li key={index} className={classNames(item.className, {'active': item.active})}>
+                        <i>{ item.icon ? item.icon : <Badge color={item.color} /> }</i>
+                        <span>{item.name}</span>
+                        {isRemovable && (
+                            <img 
+                                className="list__remove-icon" 
+                                onClick={() => removeList(item)}
+                                src={removeImg} 
+                                alt="Закрыть" 
+                            />
+                        )}
+                    </li>
+                ))
+            }
+        </ul>
     );
 };
 
