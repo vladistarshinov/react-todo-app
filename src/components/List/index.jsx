f09ff65747e77eb2fed338520acf824f22867b60
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-07-21 12:25:22 +0300

    #8 List, Tasks (routing, remove/edit task, task status)

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,53 +1,58 @@
 import React from 'react';
-import axios from 'axios';
 import classNames from 'classnames';
+import axios from 'axios';
 
-import Badge from '../Badge'
+import removeSvg from '../../assets/img/remove.svg';
 
-import removeImg from '../../assets/img/remove.svg';
+import Badge from '../Badge';
 
 import './List.scss';
 
-const List = ({ items, isRemovable, click, onRemove, clickItem, activeList }) => {
-
-    const removeList = item => {
-        if (window.confirm('Вы действительно хотите удалить список?')) {
-            axios.delete('http://localhost:3001/lists/' + item.id)
-                .then(() => {
-                    onRemove(item.id);
-                });
-        }
+const List = ({
+  items,
+  isRemovable,
+  onClick,
+  onRemove,
+  onClickItem,
+  activeItem
+}) => {
+  const removeList = item => {
+    if (window.confirm('Вы действительно хотите удалить список?')) {
+      axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
+        onRemove(item.id);
+      });
     }
+  };
 
-    return (
-        <ul onClick={click} className="list">
-            {
-                items.map((item, index) => (
-                    <li 
-                        key={index} 
-                        onClick={clickItem ? () => clickItem(item) : null}
-                        className={classNames(item.className, { 
-                            active: item.active ? item.active : activeList && activeList.id === item.id 
-                        })}
-                    >
-                        <i>{ item.icon ? item.icon : <Badge color={item.color.name} /> }</i>
-                        <span>
-                            {item.name} 
-                            {item.tasks && ` (${item.tasks.length})`} 
-                        </span>
-                        {isRemovable && (
-                            <img 
-                                className="list__remove-icon" 
-                                onClick={() => removeList(item)}
-                                src={removeImg} 
-                                alt="Закрыть" 
-                            />
-                        )}
-                    </li>
-                ))
-            }
-        </ul>
-    );
+  return (
+    <ul onClick={onClick} className="list">
+      {items.map((item, index) => (
+        <li
+          key={index}
+          className={classNames(item.className, {
+            active: item.active
+              ? item.active
+              : activeItem && activeItem.id === item.id
+          })}
+          onClick={onClickItem ? () => onClickItem(item) : null}
+        >
+          <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
+          <span>
+            {item.name}
+            {item.tasks && ` (${item.tasks.length})`}
+          </span>
+          {isRemovable && (
+            <img
+              className="list__remove-icon"
+              src={removeSvg}
+              alt="Remove icon"
+              onClick={() => removeList(item)}
+            />
+          )}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default List;
--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,66 +1,65 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-import editImg from '../../assets/img/edit.svg';
-
-import AddTask from './AddTask';
+import editSvg from '../../assets/img/edit.svg';
 
 import './Tasks.scss';
 
-const Tasks = ({ list, editTitle, onAddTask }) => {
-    
-    const renameTitle = () => {
-        const newTitle = window.prompt('Новое название списка', list.name);
-        newTitle && editTitle(list.id, newTitle);
-        axios.patch('http://localhost:3001/lists/' + list.id, {
-            name: newTitle
-        }).catch(() => {
-            alert('Не удалось обновить название списка!');
+import AddTaskForm from './AddTaskForm';
+import Task from './Task';
+
+const Tasks = ({
+  list,
+  onEditTitle,
+  onAddTask,
+  onRemoveTask,
+  onEditTask,
+  onCompleteTask,
+  withoutEmpty
+}) => {
+  const editTitle = () => {
+    const newTitle = window.prompt('Название списка', list.name);
+    if (newTitle) {
+      onEditTitle(list.id, newTitle);
+      axios
+        .patch('http://localhost:3001/lists/' + list.id, {
+          name: newTitle
+        })
+        .catch(() => {
+          alert('Не удалось обновить название списка');
         });
-    };
-    
-    return (
-        <div className="tasks">
-            <h2 className="tasks__title">
-                {list.name}
-                <img 
-                    onClick={renameTitle}
-                    src={editImg} 
-                    alt="Редактировать" 
-                />
-            </h2>   
-            <div className="tasks__items">
-                {!list.tasks.length && <h2>Задачи отсутствуют</h2>}
-                { list.tasks.map(task => (
-                    <div key={task.id} className="tasks__items-row">
-                        <div className="checkbox">
-                            <input id={`check-${task.id}`} type="checkbox" />
-                            <label htmlFor={`check-${task.id}`}>
-                                <svg 
-                                    width="11" 
-                                    height="8" 
-                                    viewBox="0 0 11 8" 
-                                    fill="none" 
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path 
-                                        d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" 
-                                        stroke="#000" 
-                                        strokeWidth="1.5" 
-                                        strokeLinecap="round" 
-                                        strokeLinejoin="round" 
-                                    />
-                                </svg>
-                            </label>
-                        </div>
-                        <input readOnly value={task.text} />
-                    </div>
-                ))}
-                <AddTask list={list} onAddTask={onAddTask} />
-                
-            </div>
-        </div>
-    );
+    }
+  };
+
+  return (
+    <div className="tasks">
+      <Link to={`/lists/${list.id}`}>
+        <h2 style={{ color: list.color.hex }} className="tasks__title">
+          {list.name}
+          <img onClick={editTitle} src={editSvg} alt="Edit icon" />
+        </h2>
+      </Link>
+
+      <div className="tasks__items">
+        {!withoutEmpty && list.tasks && !list.tasks.length && (
+          <h2>Задачи отсутствуют</h2>
+        )}
+        {list.tasks &&
+          list.tasks.map(task => (
+            <Task
+              key={task.id}
+              list={list}
+              onEdit={onEditTask}
+              onRemove={onRemoveTask}
+              onComplete={onCompleteTask}
+              {...task}
+            />
+          ))}
+        <AddTaskForm key={list.id} list={list} onAddTask={onAddTask} />
+      </div>
+    </div>
+  );
 };
 
 export default Tasks;
